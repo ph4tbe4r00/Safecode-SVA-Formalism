77313ce06d4fbcf9c8a9e144d095a96dc8fcbe7d
Daniel Huang <daneh0000@yahoo.com>
2011-07-19 13:33:43 -0400

    stuff

--- sva_dynamic.v
+++ sva_dynamic.v
@@ -41,7 +41,58 @@ Inductive stmt_step : State_stmt -> State_stmt -> Prop :=
   | R4 : forall venv l e1 e2 venv' l' e1',
       state_e venv l e1 ==>e venv' l' e1' ->
       state_s venv l (Store e1 e2) ==>s state_s venv l' (Store e1' e2)
-  (* TODO: a lot more rules ... *)
+  | R5 : forall venv l e v v' venv' l' e',
+      v = Val v' ->
+      state_e venv l e ==>e state_e venv' l' e' ->
+      state_s venv l (Store v e) ==>s state_s venv' l' (Store v e')
+  | R5char : forall venv l e v v' venv' l' e',
+      v = Val v' ->
+      state_e venv l e ==>e state_e venv' l' e' ->
+      state_s venv l (Storec v e) ==>s state_s venv' l' (Storec v e')
+  (* R6 needs update ... *)
+  | R7 : forall venv l e1 e2 e3 venv' l' e1',
+      state_e venv l e1 ==>e state_e venv' l' e1' ->
+      state_s venv l (StoreToU e1 e2 e3) ==>s state_s venv' l' (StoreToU e1' e2 e3)
+  | R7char : forall venv l e1 e2 e3 venv' l' e1',
+      state_e venv l e1 ==>e state_e venv' l' e1' ->
+      state_s venv l (StorecToU e1 e2 e3) ==>s state_s venv' l' (StorecToU e1' e2 e3)
+  | R8 : forall venv l v1 v1' e2 e3 venv' l' e2',
+      v1 = Val v1' ->
+      state_e venv l e2 ==>e state_e venv' l' e2' ->
+      state_s venv l (StoreToU v1 e2 e3) ==>s state_s venv' l' (StoreToU v1 e2' e3)
+  | R8char : forall venv l v1 v1' e2 e3 venv' l' e2',
+      v1 = Val v1' ->
+      state_e venv l e2 ==>e state_e venv' l' e2' ->
+      state_s venv l (StorecToU v1 e2 e3) ==>s state_s venv' l' (StorecToU v1 e2' e3)
+  | R9 : forall venv l v1 v1' v2 v2' e3 venv' l' e3',
+      v1 = Val v1' ->
+      v2 = Val v2' ->
+      state_e venv l e3 ==>e state_e venv' l' e3' ->
+      state_s venv l (StoreToU v1 v2 e3) ==>s state_s venv' l' (StoreToU v1 v2 e3')
+  | R9char : forall venv l v1 v1' v2 v2' e3 venv' l' e3',
+      v1 = Val v1' ->
+      v2 = Val v2' ->
+      state_e venv l e3 ==>e state_e venv' l' e3' ->
+      state_s venv l (StorecToU v1 v2 e3) ==>s state_s venv' l' (StorecToU v1 v2 e3')
+  (* R10 need update ... *)
+  (*
+  | R11 : forall venv l v1 v1' v2 v2' rho,
+      v1 = Val v1' ->
+      v2 = Val v2' ->
+      state_s venv l (StorecToU (Region_v rho) v1 v2) ==>s state_s venv
+  *)
+  | R12 : forall venv l e1 e2 venv' l' e1',
+      state_e venv l e1 ==>e state_e venv' l' e1' ->
+      state_s venv l (PoolFree e1 e2) ==>s state_s venv' l' (PoolFree e1' e2)
+  | R13 : forall venv l v v' e2 venv' l' e2',
+      v = Val v' ->
+      state_e venv l e2 ==>e state_e venv' l' e2' ->
+      state_s venv l (PoolFree v e2) ==>s state_s venv' l' (PoolFree v e2')
+  (* R14 and R15 need union ... *)
+  | R16 : forall venv l s venv' l' s' rho,
+      state_s venv l s ==>s state_s venv' l' s' ->
+      state_s venv l (PoolPop s rho) ==>s state_s venv' l' (PoolPop s' rho)
+  (* R17 needs union *)
 where " s '==>s' s' " := (stmt_step s s')
 
 with stmt_exp : State_exp -> State_exp -> Prop :=
