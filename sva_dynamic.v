2cf4d5c56efb6abc64de625212d670ace225856a
Daniel Huang <daneh0000@yahoo.com>
2011-07-20 02:44:33 -0400

    more rules

--- sva_dynamic.v
+++ sva_dynamic.v
@@ -1,4 +1,5 @@
 Require Export sva_ast.
+Require Export ListSet.
 
 (* We could have formalized ES or exp + stmt, but sums are annoying ... 
  * Right now, we can get away with partial_map's because we represent
@@ -10,19 +11,33 @@ Require Export sva_ast.
  * case or we're screwed. 
  *)
 
-Definition RS := partial_map value.
+Definition RegionStore := partial_map value. (* var -> value *)
+Definition FreeList := list value. (* list of addresses, right now its a hack for R14/R15 *)
+
+Record RegionT : Type := region_t {
+  F : FreeList;
+  RS : RegionStore
+}.
 
 Record State_exp : Type := state_e {
   VEnv_exp : partial_map value; (* var -> value *)
-  L_exp : partial_map RS; (* nodevar -> region store *)
+  L_exp : partial_map RegionT; (* nodevar -> RegionT *)
   ES_exp : exp (* expression *)
 }.
 
 Record State_stmt : Type := state_s {
   VEnv_stmt : partial_map value; (* var -> value *)
-  L_stmt : partial_map RS; (* nodevar -> region store *)
+  L_stmt : partial_map RegionT; (* nodevar -> RegionT *)
   ES_stmt : stmt (* statement *)
 }.
+
+(* hack identity function right now ... *)
+Definition updateR6 (l : partial_map RegionT) (v1 : value) (v2 : value) :=
+  l.
+
+(* hack just returning uninit ... *)
+Definition getvalue (l : partial_map RegionT) (v : value) :=
+  Val Uninit.
 
 (* Small-step relations for dynamic checks *)
 Reserved Notation " e '==>e' e' " (at level 40).
@@ -35,64 +50,82 @@ Inductive stmt_step : State_stmt -> State_stmt -> Prop :=
   | R2 : forall venv l e x venv' l' e',
       state_e venv l e ==>e state_e venv' l' e' ->
       state_s venv l (Assign x e) ==>s state_s venv' l' (Assign x e')
-  | R3 : forall venv l x v v',
-      v = Val v' ->
-      state_s venv l (Assign x v) ==>s state_s (extend venv x v') l Epsilon
+  | R3 : forall venv l x v,
+      value_v v ->
+      state_s venv l (Assign x (Val v)) ==>s state_s (extend venv x v) l Epsilon
   | R4 : forall venv l e1 e2 venv' l' e1',
       state_e venv l e1 ==>e venv' l' e1' ->
       state_s venv l (Store e1 e2) ==>s state_s venv l' (Store e1' e2)
-  | R5 : forall venv l e v v' venv' l' e',
-      v = Val v' ->
+  | R5 : forall venv l e v venv' l' e',
+      value_v v ->
       state_e venv l e ==>e state_e venv' l' e' ->
-      state_s venv l (Store v e) ==>s state_s venv' l' (Store v e')
-  | R5char : forall venv l e v v' venv' l' e',
-      v = Val v' ->
+      state_s venv l (Store (Val v) e) ==>s state_s venv' l' (Store (Val v) e')
+  | R5char : forall venv l e v venv' l' e',
+      value_v v ->
       state_e venv l e ==>e state_e venv' l' e' ->
-      state_s venv l (Storec v e) ==>s state_s venv' l' (Storec v e')
-  (* R6 needs update ... *)
+      state_s venv l (Storec (Val v) e) ==>s state_s venv' l' (Storec (Val v) e')
+  | R6 : forall venv l v1 v2 n rho,
+      value_v v1 ->
+      v1 = (Int n) \/ v1 = (Region rho) ->
+      value_v v2 ->
+      state_s venv l (Store (Val v2) (Val v1)) ==>s state_s venv (updateR6 l v1 v2) Epsilon
+  | R6char : forall venv l v1 v2 n rho,
+      value_v v1 ->
+      v1 = (Int n) \/ v1 = (Region rho) ->
+      value_v v2 ->
+      state_s venv l (Storec (Val v2) (Val v1)) ==>s state_s venv (updateR6 l v1 v2) Epsilon
   | R7 : forall venv l e1 e2 e3 venv' l' e1',
       state_e venv l e1 ==>e state_e venv' l' e1' ->
       state_s venv l (StoreToU e1 e2 e3) ==>s state_s venv' l' (StoreToU e1' e2 e3)
   | R7char : forall venv l e1 e2 e3 venv' l' e1',
       state_e venv l e1 ==>e state_e venv' l' e1' ->
       state_s venv l (StorecToU e1 e2 e3) ==>s state_s venv' l' (StorecToU e1' e2 e3)
-  | R8 : forall venv l v1 v1' e2 e3 venv' l' e2',
-      v1 = Val v1' ->
+  | R8 : forall venv l v1 e2 e3 venv' l' e2',
+      value_v v1 ->
       state_e venv l e2 ==>e state_e venv' l' e2' ->
-      state_s venv l (StoreToU v1 e2 e3) ==>s state_s venv' l' (StoreToU v1 e2' e3)
-  | R8char : forall venv l v1 v1' e2 e3 venv' l' e2',
-      v1 = Val v1' ->
+      state_s venv l (StoreToU (Val v1) e2 e3) ==>s state_s venv' l' (StoreToU (Val v1) e2' e3)
+  | R8char : forall venv l v1 e2 e3 venv' l' e2',
+      value_v v1 ->
       state_e venv l e2 ==>e state_e venv' l' e2' ->
-      state_s venv l (StorecToU v1 e2 e3) ==>s state_s venv' l' (StorecToU v1 e2' e3)
-  | R9 : forall venv l v1 v1' v2 v2' e3 venv' l' e3',
-      v1 = Val v1' ->
-      v2 = Val v2' ->
+      state_s venv l (StorecToU (Val v1) e2 e3) ==>s state_s venv' l' (StorecToU (Val v1) e2' e3)
+  | R9 : forall venv l v1 v2 e3 venv' l' e3',
+      value_v v1 ->
+      value_v v2 ->
       state_e venv l e3 ==>e state_e venv' l' e3' ->
-      state_s venv l (StoreToU v1 v2 e3) ==>s state_s venv' l' (StoreToU v1 v2 e3')
-  | R9char : forall venv l v1 v1' v2 v2' e3 venv' l' e3',
-      v1 = Val v1' ->
-      v2 = Val v2' ->
+      state_s venv l (StoreToU (Val v1) (Val v2) e3) ==>s state_s venv' l' (StoreToU (Val v1) (Val v2) e3')
+  | R9char : forall venv l v1 v2 e3 venv' l' e3',
+      value_v v1 ->
+      value_v v2 ->
       state_e venv l e3 ==>e state_e venv' l' e3' ->
-      state_s venv l (StorecToU v1 v2 e3) ==>s state_s venv' l' (StorecToU v1 v2 e3')
+      state_s venv l (StorecToU (Val v1) (Val v2) e3) ==>s state_s venv' l' (StorecToU (Val v1) (Val v2) e3')
   (* R10 need update ... *)
-  (*
-  | R11 : forall venv l v1 v1' v2 v2' rho,
-      v1 = Val v1' ->
-      v2 = Val v2' ->
-      state_s venv l (StorecToU (Region_v rho) v1 v2) ==>s state_s venv
-  *)
+  | R11 : forall venv l v1 v2 rho,
+      value_v v1 ->
+      value_v v2 ->
+      state_s venv l (StorecToU (Val (Region rho)) (Val v1) (Val v2)) ==>s state_s venv (updateR6 l v1 v2) Epsilon
   | R12 : forall venv l e1 e2 venv' l' e1',
       state_e venv l e1 ==>e state_e venv' l' e1' ->
       state_s venv l (PoolFree e1 e2) ==>s state_s venv' l' (PoolFree e1' e2)
-  | R13 : forall venv l v v' e2 venv' l' e2',
-      v = Val v' ->
+  | R13 : forall venv l v e2 venv' l' e2',
+      value_v v ->
       state_e venv l e2 ==>e state_e venv' l' e2' ->
-      state_s venv l (PoolFree v e2) ==>s state_s venv' l' (PoolFree v e2')
-  (* R14 and R15 need union ... *)
+      state_s venv l (PoolFree (Val v) e2) ==>s state_s venv' l' (PoolFree (Val v) e2')
+  | R14 : forall venv l v n rho f rs,
+      value_v v ->
+      v = (Int n) \/ v = (Region rho) ->
+      l rho = Some (region_t f rs) ->
+      state_s venv l (PoolFree (Val (Region rho)) (Val v)) ==>s 
+      state_s venv (extend l rho (region_t (v::f) rs))Epsilon
+  | R15 : forall venv l rho tau x s,
+      l rho = None ->
+      state_s venv l (PoolInit rho tau x s) ==>s state_s (extend venv x (Region rho)) (extend l rho (region_t [] (empty))) (PoolPop s rho)
   | R16 : forall venv l s venv' l' s' rho,
       state_s venv l s ==>s state_s venv' l' s' ->
       state_s venv l (PoolPop s rho) ==>s state_s venv' l' (PoolPop s' rho)
-  (* R17 needs union *)
+  | R17 : forall venv l x rho r,
+      state_s (extend venv x (Region rho)) (extend l rho r) (PoolPop Epsilon rho) ==>s
+      state_s venv l Epsilon
+      (* some bs about the heap needs to be taken care of? *)
 where " s '==>s' s' " := (stmt_step s s')
 
 with stmt_exp : State_exp -> State_exp -> Prop :=
@@ -102,15 +135,15 @@ with stmt_exp : State_exp -> State_exp -> Prop :=
       state_e venv l e1 ==>e state_e venv' l' e1' ->
       state_e venv l (Binop e1 op e2) ==>e state_e venv' l' (Binop e1' op e2)
   | R20 : forall venv l v e2 op venv' l' e2',
-      Val v ->
+      value_v v ->
       state_e venv l e2 ==>e state_e venv' l' e2' ->
-      state_e venv l (Binop v op e2) ==>e state_e venv' l' (Binop v op e2')
-  | R21 : forall venv l v1 v1' v2 v2' op,
-      v1 = Val v1' ->
-      v2 = Val v2' ->
+      state_e venv l (Binop (Val v) op e2) ==>e state_e venv' l' (Binop (Val v) op e2')
+  | R21 : forall venv l v1 v2 op,
+      value_v v1 ->
+      value_v v2 ->
       (* warning ... right now this is a hack, but we don't need to do a
        * computation? *)
-      state_e venv l (Binop v1 op v2) ==>e state_e venv l v1
+      state_e venv l (Binop (Val v1) op (Val v2)) ==>e state_e venv l (Val v1)
   | R22 : forall venv l e venv' l' e',
       state_e venv l e ==>e state_e venv' l' e' ->
       state_e venv l (Load e) ==>e state_e venv' l' (Load e')
@@ -124,25 +157,27 @@ with stmt_exp : State_exp -> State_exp -> Prop :=
   | R24char : forall venv l e1 e2 venv' l' e1',
       state_e venv l e1 ==>e state_e venv' l' e1' ->
       state_e venv l (LoadcFromU e1 e2) ==>e state_e venv l (LoadcFromU e1' e2)
-  | R25 : forall venv l v v' e2 venv' l' e2',
-      v = Val v' ->
+  | R25 : forall venv l v e2 venv' l' e2',
+      value_v v ->
       state_e venv l e2 ==>e state_e venv' l' e2' ->
-      state_e venv l (LoadFromU v e2) ==>e state_e venv l (LoadFromU v e2')
-  | R25char : forall venv l v v' e2 venv' l' e2',
-      v = Val v' ->
+      state_e venv l (LoadFromU (Val v) e2) ==>e state_e venv l (LoadFromU (Val v) e2')
+  | R25char : forall venv l v e2 venv' l' e2',
+      value_v v ->
       state_e venv l e2 ==>e state_e venv' l' e2' ->
-      state_e venv l (LoadcFromU v e2) ==>e state_e venv l (LoadcFromU v e2')
+      state_e venv l (LoadcFromU (Val v) e2) ==>e state_e venv l (LoadcFromU (Val v) e2')
   (* R26 looks beastly ... *)
-  (* R27 needs getvalue ... *)
+  | R27 : forall venv l v rho,
+      value_v v ->
+      state_e venv l (LoadcFromU (Val (Region rho)) (Val v)) ==>e state_e venv l (getvalue l v)
   | R28 : forall venv l e tau,
       state_e venv l (Cast e tau) ==>e state_e venv l e
   | R29 : forall venv l e1 e2 venv' l' e1' tau,
       state_e venv l e1 ==>e state_e venv' l' e1' ->
       state_e venv l (CastI2Ptr e1 e2 tau) ==>e state_e venv' l' (CastI2Ptr e1' e2 tau)
-  | R30 : forall venv l v v' e venv' l' e' tau,
-      v = Val v' ->
+  | R30 : forall venv l v e venv' l' e' tau,
+      value_v v ->
       state_e venv l e ==>e state_e venv' l' e' ->
-      state_e venv l (CastI2Ptr v e tau) ==>e state_e venv' l' (CastI2Ptr v e' tau)
+      state_e venv l (CastI2Ptr (Val v) e tau) ==>e state_e venv' l' (CastI2Ptr (Val v) e' tau)
   (*
   | R31 : forall venv l rho v v' v'' tau,
       v = Val v' ->
@@ -152,10 +187,30 @@ with stmt_exp : State_exp -> State_exp -> Prop :=
   | R32 : forall venv l e1 e2 venv' l' e1',
       state_e venv l e1 ==>e state_e venv l' e1' ->
       state_e venv l (PoolAlloc e1 e2) ==>e state_e venv' l' (PoolAlloc e1' e2)
-  | R33 : forall venv l v v' e2 venv' l' e2',
-      v = Val v' ->
+  | R33 : forall venv l v e2 venv' l' e2',
+      value_v v ->
       state_e venv l e2 ==>e state_e venv l' e2' ->
-      state_e venv l (PoolAlloc v e2) ==>e state_e venv' l' (PoolAlloc v e2')
-  (* R34, R35, R36 need union *)
+      state_e venv l (PoolAlloc (Val v) e2) ==>e state_e venv' l' (PoolAlloc (Val v) e2')
+  | R34 : forall venv l rho f rs a,
+      state_e venv (extend l rho (region_t (a::f) rs)) (PoolAlloc (Val (Region rho)) (Val (Int 1))) ==>e
+      state_e venv (extend l rho (region_t f rs)) (Val a)
+  (* R34, R35 and R36 are ... *)
+  | R37 : forall venv l e1 e2 e3 venv' l' e1',
+      state_e venv l e1 ==>e state_e venv' l' e1' ->
+      state_e venv l (Addr e1 e2 e3) ==>e state_e venv' l' (Addr e1' e2 e3)
+  | R38 : forall venv l v1 e2 e3 venv' l' e2',
+      value_v v1 ->
+      state_e venv l e2 ==>e state_e venv' l' e2' ->
+      state_e venv l (Addr (Val v1) e2 e3) ==>e state_e venv' l' (Addr (Val v1) e2' e3)
+  | R39 : forall venv l v1 v2 e3 venv' l' e3',
+      value_v v1 ->
+      value_v v2 ->
+      state_e venv l e3 ==>e state_e venv' l' e3' ->
+      state_e venv l (Addr (Val v1) (Val v2) e3) ==>e state_e venv' l' (Addr (Val v1) (Val v2) e3')
+  (* warning hack right now ... need to make it an offset calculation *)
+  | R40 : forall venv l v1 v2 rho,
+      value_v v1 ->
+      value_v v2 ->
+      state_e venv l (Addr (Val (Region rho)) (Val v1) (Val v2)) ==>e state_e venv l (Val v1)
 where " e '==>e' e' " := (stmt_exp e e').
 
